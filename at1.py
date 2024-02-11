a19b59da75075c7134ef2800040261271ebdd749
serse3 <90264870+serse3@users.noreply.github.com>
2024-02-11 20:21:24 +0100

    Update at1.py

--- at1.py
+++ at1.py
@@ -1,3 +1,4 @@
+import tweepy
 import streamlit as st
 
 # Crea un título
@@ -5,3 +6,6 @@ st.title("Hello, this is autotwitter")
 
 # Crea un subtítulo
 st.write("This is a simple Streamlit app that displays a message.")
+
+#Api key: ywyibwUnKHOHshdzU3tvGMFDy
+#Api secret key: xagFFxDQukq1kKq7MJkD2EhQjPBxWdhJazlIPqwwgIIeTT0bCz
